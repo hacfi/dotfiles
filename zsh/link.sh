dbe5a92bc37267fdd15de1552f5b70e0309cc4a2
Philipp Wahala <philipp.wahala@gmail.com>
2015-07-21 22:56:54 +0200

    Add zsh linker & default oh-my-zsh .zshrc

--- zsh/link.sh
+++ zsh/link.sh
@@ -10,3 +10,6 @@ if [ X"$unamestr" = X'Darwin' ]; then
 elif [ X"$unamestr" = X'Linux' ]; then
   ln -s $HOME/.dotfiles/zsh/hacfi-linux.zsh $HOME/.oh-my-zsh/custom
 fi
+
+mkdir -p $HOME/.oh-my-zsh/custom/plugins/jump
+ln -s $HOME/.dotfiles/zsh/jump.plugin.zsh $HOME/.oh-my-zsh/custom/plugins/jump
